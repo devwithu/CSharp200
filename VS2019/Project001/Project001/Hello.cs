a38c74d842c5c6b53a3cc6a3e16a426c0e4a9bb2
jdj <jdj@elevisor.com>
2019-01-28 08:10:36 +0900

    Project001

---
+++ VS2019/Project001/Project001/Hello.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Jungbo.Basic
+{ 
+    public class Hello
+    {
+
+    
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("덧셈을 하려고 합니다. 두 수를 입력하세요.");
+
+            Console.Write("첫번째 수를 입력하세요: ");
+            int num1 = int.Parse(Console.ReadLine());//첫 번째 수 입력후 변환
+
+            Console.Write("두번째 수를 입력하세요: ");
+            int num2 = int.Parse(Console.ReadLine());//두 번째 수 입력후 변환
+
+            //{0}위치에 num1이, {1}위치에 num2이 들어간다.
+            Console.WriteLine("두수의 합: {0}+{1}={2}", num1, num2, num1 + num2);
+        }
+    }
+}
