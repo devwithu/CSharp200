3d87bb73380022ea5ff7b70e513efce9f008770f
jdj <jdj@elevisor.com>
2019-02-10 13:03:36 +0900

    project021

---
+++ VS2019/Project021/Project021/Program.cs
@@ -0,0 +1,14 @@
+﻿using Com.JungBo.Logics;
+
+namespace Com.JungBo.Basic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HechoCalendar hecal = new HechoCalendar();
+            //1990~2010 사이의 윤년
+            hecal.PrintLeapYear(1990, 2010);
+        }
+    }
+}
